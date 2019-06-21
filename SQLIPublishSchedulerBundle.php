7ad9ae86f16e6158349a06b3d5bef5c3194c40f2
dverner <daverner@sqli.com>
2019-06-21 17:20:38 +0200

    Bundle SQLI Publish Scheduler

--- SQLIPublishSchedulerBundle.php
+++ SQLIPublishSchedulerBundle.php
@@ -2,8 +2,16 @@
 
 namespace SQLI\PublishSchedulerBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SQLIPublishSchedulerBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
 }
